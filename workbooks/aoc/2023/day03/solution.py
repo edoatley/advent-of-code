7d0fd81c0cf73bf9566af6aa61eb5696efe1cfca
edoatleynewday <ed.oatley@newday.co.uk>
2023-12-06 17:38:24 +0000

    some progress

--- workbooks/aoc/2023/day03/solution.py
+++ workbooks/aoc/2023/day03/solution.py
@@ -51,6 +51,8 @@ def process_line(engine: list[str], index: int) -> int:
     start_index = None
     end_index = None
 
+    print(f'process_line {index=}')
+    
     part_sum = 0
 
     for c in range(len(engine[index])):
@@ -72,59 +74,77 @@ def process_line(engine: list[str], index: int) -> int:
 
     return part_sum
 
+# 0123456789
+# ...$.*....   gear_index = 5 
+# ..664.598.   C1 664 si=2 ei=4 C2 598 si=6 ei=8
+# .664.598..   C3 598 si=5 ei=7
+# .664..598.   C4 598 si=6 ei=8
+# .66412598.   C5 66412598 si=1 ei=8
+# so we need to check if the gear index is 
+# a) between the start and end index inclusive (C3, C5)
+# b) start_index == gear_index + 1 (C2, C4)
+# c) end_index == gear_index - 1 (C1)
 def extract_touching_numbers(engine: list[str], gear_index: int, check_index: int) -> list[int]:
     start_index = None
     end_index = None
-    
     numbers=[]
+
+    print(f'extract_touching_numbers {gear_index=} {check_index=} {engine[check_index]=}')
     
     for c in range(len(engine[check_index])):
+        # print(f'A {c=} {engine[check_index][c]=} {start_index=} {end_index=} {gear_index=}')
         if engine[check_index][c].isdigit():
+            # print(f'B Found digit {c=} {engine[check_index][c]=}')
             if start_index == None:
                 start_index = c
                 end_index = c
             else:
                 end_index = c
         elif start_index != None and end_index != None:
-            # TODO: check if touching gear!!!
+            # print(f'C Found non-digit {c=} {engine[check_index][c]=} {start_index=} {end_index=} {gear_index=}')
             if (
-                (start_index < gear_index and end_index >= gear_index - 1 ) or
-                (start_index >= gear_index + 1 and end_index > gear_index )
+                (start_index <= gear_index and end_index >= gear_index  ) or
+                (start_index == gear_index + 1 or end_index == gear_index - 1 )
             ):
-                print(f'Adding value from {engine[check_index]=} of {engine[check_index][start_index:end_index+1]=}')
+                # print(f'D {start_index=} {end_index=} {gear_index=} {engine[check_index]=}')
                 numbers.append(int(engine[check_index][start_index:end_index+1]))
-                start_index = None
-                end_index = None
+            start_index = None
+            end_index = None
                 
     if start_index != None and end_index != None:
         if (
-            (start_index < gear_index and end_index >= gear_index - 1 ) or
-            (start_index >= gear_index + 1 and end_index > gear_index )
+            (start_index <= gear_index and end_index >= gear_index  ) or
+            (start_index == gear_index + 1 or end_index == gear_index - 1 )
         ):
             numbers.append(int(engine[check_index][start_index:end_index+1]))
     
     return numbers
             
-            
 
 def get_gear_parts(engine: list[str], engine_index: int, gear_index: int) -> list[int]:
     parts=[]
     row_length = len(engine[engine_index])
+
+    print(f'get_gear_parts(): {engine[engine_index]=} {gear_index=} {engine[engine_index-1]=} {engine[engine_index+1]=}')
     
     # check line above
     if engine_index > 0:
         parts.extend(extract_touching_numbers(engine, gear_index, engine_index-1))
+        print(f'get_gear_parts above {parts=}')
     
     # check line below
     if engine_index + 1 < row_length:
         parts.extend(extract_touching_numbers(engine, gear_index, engine_index+1))
+        print(f'get_gear_parts below {parts=}')
     
     # check before
     test_idx = gear_index - 1
     if test_idx >= 0 and engine[engine_index][test_idx].isdigit():
         while test_idx >= 0 and engine[engine_index][test_idx].isdigit():
+            print(f'{test_idx=} {engine[engine_index][test_idx]=}')
             test_idx -= 1
-        parts.extend(int(engine[engine_index][test_idx:gear_index]))
+        parts.append(int(engine[engine_index][test_idx+1:gear_index]))
+        print(f'get_gear_parts left {parts=}')
     
     # check after
     test_idx = gear_index + 1
@@ -132,7 +152,9 @@ def get_gear_parts(engine: list[str], engine_index: int, gear_index: int) -> lis
         while test_idx < row_length and engine[engine_index][test_idx].isdigit():
             test_idx += 1
         parts.extend(int(engine[engine_index][test_idx:gear_index]))
+        print(f'get_gear_parts right {parts=}')
     
+    print(f'get_gear_parts returning {parts=}')
     return parts
     
 def process_gear(engine: list[str], index: int):
--- workbooks/aoc/2023/day03/test_solution.py
+++ workbooks/aoc/2023/day03/test_solution.py
@@ -3,46 +3,61 @@ import solution
 
 
 class TestGetAreaRectangle(unittest.TestCase):
-    
-    # def test_is_symbol_number(self):
-    #     for i in range(10):
-    #         self.assertEqual(solution.is_symbol(f"{i}"), False)
+    def test_is_symbol_number(self):
+        for i in range(10):
+            self.assertEqual(solution.is_symbol(f"{i}"), False)
 
-    # def test_is_symbol_stop(self):
-    #     self.assertEqual(solution.is_symbol(f"."), False)
+    def test_is_symbol_stop(self):
+        self.assertEqual(solution.is_symbol(f"."), False)
 
-    # def test_is_symbol(self):
-    #     for s in ["*", "#", "+", "*"]:
-    #         self.assertEqual(solution.is_symbol(s), True)
+    def test_is_symbol(self):
+        for s in ["*", "#", "+", "*"]:
+            self.assertEqual(solution.is_symbol(s), True)
 
-    # def test_check_adjacent_simple(self):
-    #     engine = ["467..114..", "...*......", "..35..633.", "......#..."]
+    def test_check_adjacent_simple(self):
+        engine = ["467..114..", "...*......", "..35..633.", "......#..."]
 
-    #     self.assertEqual(solution.check_adjacent_row(engine, 1, 0, 2), True)
-    #     self.assertEqual(solution.check_adjacent_row(engine, 1, 5, 7), False)
-    #     self.assertEqual(solution.check_adjacent_row(engine, 1, 2, 3), True)
-    #     self.assertEqual(solution.check_adjacent_row(engine, 3, 6, 8), True)
+        self.assertEqual(solution.check_adjacent_row(engine, 1, 0, 2), True)
+        self.assertEqual(solution.check_adjacent_row(engine, 1, 5, 7), False)
+        self.assertEqual(solution.check_adjacent_row(engine, 1, 2, 3), True)
+        self.assertEqual(solution.check_adjacent_row(engine, 3, 6, 8), True)
 
-    # def test_check_for_symbol(self):
-    #     engine = ["467..114..", "...*......", "..35..633.", "......#..."]
+    def test_check_for_symbol(self):
+        engine = ["467..114..", "...*......", "..35..633.", "......#..."]
 
-    #     self.assertEqual(solution.check_for_symbol(engine, 0, 0, 2), 467)
-    #     self.assertEqual(solution.check_for_symbol(engine, 0, 5, 7), 0)
-    #     self.assertEqual(solution.check_for_symbol(engine, 2, 2, 3), 35)
-    #     self.assertEqual(solution.check_for_symbol(engine, 2, 6, 8), 633)
+        self.assertEqual(solution.check_for_symbol(engine, 0, 0, 2), 467)
+        self.assertEqual(solution.check_for_symbol(engine, 0, 5, 7), 0)
+        self.assertEqual(solution.check_for_symbol(engine, 2, 2, 3), 35)
+        self.assertEqual(solution.check_for_symbol(engine, 2, 6, 8), 633)
 
-    # def test_process_line(self):
-    #     engine = ["467..114..", "...*......", "..35..633.", "......#..."]
-    #     self.assertEqual(solution.process_line(engine, 0), 467)
-    #     self.assertEqual(solution.process_line(engine, 1), 0)
-    #     self.assertEqual(solution.process_line(engine, 2), 668)
-    #     self.assertEqual(solution.process_line(engine, 3), 0)
+    def test_process_line(self):
+        engine = ["467..114..", "...*......", "..35..633.", "......#..."]
+        self.assertEqual(solution.process_line(engine, 0), 467)
+        self.assertEqual(solution.process_line(engine, 1), 0)
+        self.assertEqual(solution.process_line(engine, 2), 668)
+        self.assertEqual(solution.process_line(engine, 3), 0)
 
-    # def test_solve(self):
-    #     self.assertEqual(solution.solve("test.txt"), 4361)
+    def test_solve(self):
+        self.assertEqual(solution.solve("test.txt"), 4361)
 
     def test_solve2(self):
+        print('solution.solve2("test.txt")')
         self.assertEqual(solution.solve2("test.txt"), 467835)
+
+    def test_extract_touching_numbers(self):
+        engine = [
+            "467..114..",
+            "...*......",
+            "..35..633.",
+            "......#...",
+            "617*......",
+            ".....+.58.",
+            "..592.....",
+            "......755.",
+            "...$.*....",
+            ".664.598..",
+        ]
+        self.assertEqual(solution.extract_touching_numbers(engine, 5, 9), [598])
 
 
 unittest.main()
