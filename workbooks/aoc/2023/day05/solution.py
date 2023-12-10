e0e8a527258e1e2083e8730eee813bccfb1d2ad4
edoatley <edoatley@gmail.com>
2023-12-10 18:30:04 +0000

    savepoint

--- workbooks/aoc/2023/day05/solution.py
+++ workbooks/aoc/2023/day05/solution.py
@@ -1,14 +1,44 @@
-def get_mapper(source:int, target:int, length:int):
-    def mapper(value:int):
-        if value >= source and value <= source + length:
-            delta = source - target
-            return value - delta
-        return None
-    return mapper
+def process_map_line(maps: list[list], map_index : int, line : str):
+    source, dest, length = [int(s) for s in line.split(' ')]
+    delta = dest - source
+    return (int(source), int(source) + int(length)), delta
+
+def read_file_data_structure(filename: str) -> (list[int], list[list]):
+    seeds = None
+    maps = []
+    maps_index = -1
+    
+    with open(filename, "r") as f:
+        for original_line in f.readlines():
+            if original_line.strip == '':
+                continue
+            elif original_line.startswith('seeds: '):
+                seeds = [int(s) for s in original_line.split(':')[1].strip().split(' ')]
+            elif original_line.contains('map:'):
+                maps_index += 1
+                maps[maps_index] = []
+            else:
+                process_map_line(maps, maps_index, original_line.strip())
+    
+    return seeds, maps
+                    
+def map_seed()                
+
+                
 
 
 def solve(filename: str):
-    pass
+    seeds, maps = read_file_data_structure(filename)
+    
+    min_location = None
+    for s in seeds:
+        location = map_seed(s, maps)
+        print(f'{s=} ==> {location=}')
+        if min_location == None or location < min_location:
+            min_location = location
+            print(f'{min_location=}')
+            
+
 
 def solve2(filename: str):
     pass
